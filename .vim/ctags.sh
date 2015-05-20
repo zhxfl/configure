a258d23126e7040fc6782ebad2166aa1cd873d1d
zhxfl <huanxinzheng@tuputech.com>
2015-05-20 10:22:11 +0800

    add vim color

--- .vim/ctags.sh
+++
@@ -1,4 +0,0 @@
-find /usr/local/include/ -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" -o -name "*.cu" -o -name "*.cuh" > cscope.files
-cscope -bkq -i cscope.files
-ctags -R `cat cscope.files`
-
